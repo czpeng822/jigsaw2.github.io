13a6ca380653c3ebac17d404356cea6b8aa4da25
czpeng822 <153256821+czpeng822@users.noreply.github.com>
2024-01-10 12:21:30 -0500

    change the frame

--- jigsaw.js
+++ jigsaw.js
@@ -1,20 +1,14 @@
 class Jigsaw{
-	construtor(numpieces){
-		this.numpieces = numpieces;
+	constructor(){
+		this.piecesnum = 1;
+		this.row = 1
 	}
-	var row = Math.sqrt(numpieces)
-	if(row%1==0){
-		var i;
-		for(i=0,i<numpieces,i++){
-			var newDiv = document.createElement('div');
-            newDiv.setAttribute('class', 'row'+i);
-            newDiv.setAttribute('id', 'row'+i);
-            document.body.appendChild(newDiv);
-            newDiv.body
 
+	setJigsaw(piecesnum){
+		this.piecesnum=piecesnum;
+		this.row = Math.sqrt(this.piecesnum)
+		if(this.row%1 != 0){
+			console.log("input pieces number error,please input the number which is inter squrt")
 		}
-	}
-	else{
-		console ("input numpieces error,please input the number which is inter squrt")
 	}
 }
