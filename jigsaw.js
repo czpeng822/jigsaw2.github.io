eace3889fc91def6d286694255a89aa468f88d20
czpeng822 <153256821+czpeng822@users.noreply.github.com>
2024-01-11 17:02:13 -0500

    add cheack

--- jigsaw.js
+++ jigsaw.js
@@ -23,6 +23,9 @@ class Jigsaw{
         img.className =`img`;
         img.alt = `image${i}`;
         img.draggable = true;
+				img.ondragstart = this.drag.bind(this);
+				img.ondrop = this.drop.bind(this);
+				img.ondragover = this.allowDrop.bind(this);
         container.appendChild(img); // 将图片添加到容器中
       }
     } else {
@@ -41,6 +44,9 @@ class Jigsaw{
 				img.className =`img`;
 				img.alt = `image${i}`;
 				img.draggable = true;
+				img.ondragstart = this.drag.bind(this);
+				img.ondrop = this.drop.bind(this);
+				img.ondragover = this.allowDrop.bind(this);
 				image_puzzle.appendChild(img); // 将图片添加到容器中
 			}
 		} else {
@@ -65,4 +71,58 @@ class Jigsaw{
 		    `;
 		    document.head.appendChild(style); // 将样式添加到文档的头部
 		  }
+
+	allowDrop(ev)
+		{
+		    ev.preventDefault();
+		}
+
+	drag(ev)
+		{
+		    ev.dataTransfer.setData("Image",ev.target.id);
+
+		}
+
+		drop(ev) {
+		    ev.preventDefault();
+		    var data = ev.dataTransfer.getData("Image");
+		    if (document.getElementById(data).src.includes("none.jpeg")) {
+		      return;
+		    } else if (ev.target.src.includes("none.jpeg")) {
+		      ev.target.src = document.getElementById(data).src;
+		      ev.target.alt = document.getElementById(data).alt;
+		      ev.target.width = document.getElementById(data).width;
+		      ev.target.height = document.getElementById(data).height;
+		      document.getElementById(data).src = "assets/none.jpeg";
+					setTimeout(() => {
+			      this.check();
+			    }, 50);
+		}}
+
+   check() {
+	     var jigsawall = document.querySelectorAll('.image_puzzle img');
+	     var completetag = true;
+	     jigsawall.forEach(function (jigsaw) {
+	         if (jigsaw.src.includes('none.jpeg')) {
+	             completetag = false;
+	         }
+	     });
+
+	     if (completetag) {
+	         var machtag = true;
+	         jigsawall.forEach(function (jigsaw) {
+	             if (!jigsaw.src.includes(jigsaw.id)) {
+	                 console.log(jigsaw.src+"---"+jigsaw.id);
+	                 machtag = false;
+	             }
+	         });
+
+	         if (machtag) {
+	             alert("Congratulations! You win!");
+	         }
+	     }
+	     console.log("completetag:"+completetag);
+	     console.log("machtag:"+machtag);
+	 }
+
 }
